e611edd3ff17af96ee18adbd4cfa53fb899a8646
Eriq-511 <2023bit216@std.must.ac.ug>
2026-03-16 19:07:35 +0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
         <meta name="description" content="Where developers build the foundation, Pantheon lets creators shape the experience." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body>
         <Provider store={store}>
